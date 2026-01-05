6d83a227d6688e914f9303a4c39bdace17cfc857
unknown <saurabh@logicwiseworks.com>
2026-01-05 14:14:20 +0530

    updated

--- form.js
+++ form.js
@@ -1,4 +1,5 @@
-
+var editUserIndex = null;
+var selectedRow = null
 
 
 function userFormValidation() {
@@ -13,9 +14,18 @@ function userFormValidation() {
     const userPassword = document.getElementById("user-password").value.trim();
     const userConfrimPass = document.getElementById("user-confirm-password").value.trim();
     const genderDropdown = document.getElementById("user-gender").value.trim();
+    const English = document.getElementById("lang-en").checked
+    const hindi = document.getElementById("lang-hi").checked
+    const gujarati = document.getElementById("lang-gu").checked
+    const languagesKnownEnglish = document.getElementById("chk-en").checked
+    const languagesKnownHindi = document.getElementById("chk-hi").checked
+    const languagesKnownGujarati = document.getElementById("chk-gu").checked
+    const languagesKnownMarathi = document.getElementById("chk-mr").checked
 
     if (fullName && emailId && contactNumber && schoolName
-        && Age && userPassword && userConfrimPass && Cgpa && genderDropdown) {
+        && Age && userPassword && userConfrimPass && Cgpa !== "0" &&
+        genderDropdown !== "Choosegender" && !isDuplicateEmail(editUserIndex) && (English || hindi || gujarati)
+        && (languagesKnownEnglish || languagesKnownHindi || languagesKnownGujarati || languagesKnownMarathi)) {
 
         submitButton.disabled = false;
         return true;
@@ -27,6 +37,8 @@ function userFormValidation() {
 
 
 };
+
+
 
 function validateUserName() {
     const userName = document.getElementById("user-name").value.trim();
@@ -43,7 +55,6 @@ function validateUserName() {
 
 }
 
-//for email
 function emailValidation() {
     const userEmail = document.getElementById("email-id");
     const emailError = document.getElementById("email-error");
@@ -55,15 +66,19 @@ function emailValidation() {
         emailError.textContent = "Enter valid email"
         return false;
     }
-    else {
-        emailError.textContent = "";
-        return true;
+    if (isDuplicateEmail()) {
+        emailError.textContent = "Email already exists"
+        console.log("duplicate email")
+        return false;
     }
+
+    emailError.textContent = "";
+    return true;
 
 }
 
 
-//for mobile
+
 
 function contactNumberValidation() {
     const contactNumber = document.getElementById("contact-number");
@@ -219,14 +234,10 @@ function checkBox() {
 
 function dropDown() {
 
-    // Get the dropdown element
+
     const choosedGender = document.getElementById("user-gender");
     userFormValidation()
-
-    // Get the selected value
     const selectedValue = choosedGender.value;
-
-    // Validate selection
     if (selectedValue === "Choosegender") {
         document.getElementById("gender-error").textContent = "Please select a Gender.";
         console.log("hello")
@@ -250,7 +261,13 @@ function addRow(user) {
     newRow.insertCell(4).innerText = user.email;
     newRow.insertCell(5).innerText = user.school;
     newRow.insertCell(6).innerText = user.schoolCgpa;
-    // newRow.insertCell(7).innerText =
+    newRow.insertCell(7).innerText = user.primaryLanguage;
+    newRow.insertCell(8).innerText = user.languagesKnown
+    newRow.insertCell(9).innerHTML = `<a  onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`
+
+
+
 
 
 
@@ -263,47 +280,6 @@ function pageLoad() {
     storedData.forEach(users => addRow(users));
     // localStorage.clear()
 }
-
-// function add3row(data) {
-//     const tableBody = document.getElementById("user-table")
-//     tableBody.innerHTML = ""
-//     data.forEach(item => {
-//         const row = document.createElement("tr");
-//         row.innerHTML = `<td>${String(item.name)}</td><td>${String(item.age)}</td>
-//         <td>${String(item.age)}</td><td>${String(item.age)}</td><td>${String(item.age)}</td>
-//         <td>${String(item.age)}</td><td>${String(item.age)}</td>`;
-//         tableBody.appendChild(row);
-//     })
-
-
-// }
-
-
-function addRow3() {
-
-
-    const tableBody = document.getElementById("user-table").querySelector("tbody");
-    const newRow = document.createElement("tr");
-
-    // Create 3 cells dynamically
-    tableBody.rows[1].
-
-    tableBody.appendChild(newRow);
-
-}
-
-
-
-// function toaddFirst3Data() {
-//     const storedData = JSON.parse(localStorage.getItem("users")) || [];
-//     const first3data = storedData.slice(0, 3)
-//     console.log(first3data);
-//     first3data.forEach(users => add3row(users))
-
-//     // first3data.forEach(users => add3row(users))
-//     // storedData.forEach(users => addRow(users));
-
-// }
 
 
 function addToTable() {
@@ -369,6 +345,8 @@ function toSaveLocalStorage() {
     const schoolName = document.getElementById("school-name").value.trim();
     const Cgpa = document.getElementById('High-school-cgpa').value.trim();
     const Age = document.getElementById("user-age").value.trim();
+    const primaryLanguage = getPrimaryLanguage();
+    const languagesKnown = getLanguagesKnown();
 
     const newUser = {
         userName: fullName,
@@ -377,18 +355,29 @@ function toSaveLocalStorage() {
         contactNum: contactNumber,
         school: schoolName,
         schoolCgpa: Cgpa,
-        userAge: Age
+        userAge: Age,
+        primaryLanguage: primaryLanguage,
+        languagesKnown: languagesKnown
     }
-
 
     let users = JSON.parse(localStorage.getItem("users")) || [];
-    if (!Array.isArray(users)) {
-        users = [];
+
+    if (editUserIndex === null) {
+
+        users.push(newUser);
+        console.log("User added");
+    } else {
+
+        users[editUserIndex] = newUser;
+
+        console.log("User updated");
     }
-    users.push(newUser);
+
     localStorage.setItem("users", JSON.stringify(users));
     console.log("User saved");
     console.log(newUser.userName)
+
+    // newUser.forEach(users => addRow(users));
 
 
 }
@@ -401,7 +390,11 @@ function toSaveDefoultData() {
         contactNum: "9999999999",
         school: "Public School",
         schoolCgpa: "7",
-        userAge: "32"
+        userAge: "32",
+        primaryLanguage: "English",
+        languagesKnown: ["Hindi", "Marathi"]
+
+
     }
     let user2 = {
         userName: "John Doe",
@@ -410,7 +403,10 @@ function toSaveDefoultData() {
         contactNum: "9876543210",
         school: "Green Valley High",
         schoolCgpa: "8.5",
-        userAge: "28"
+        userAge: "28",
+        primaryLanguage: "English",
+        languagesKnown: ["Hindi", "Marathi"]
+
     }
     let user3 = {
         userName: "Jane Smith",
@@ -419,7 +415,10 @@ function toSaveDefoultData() {
         contactNum: "9123456789",
         school: "Sunrise Public School",
         schoolCgpa: "9.1",
-        userAge: "25"
+        userAge: "25",
+        primaryLanguage: "English",
+        languagesKnown: ["Hindi", "Marathi"]
+
     }
 
 
@@ -433,28 +432,190 @@ function toSaveDefoultData() {
     localStorage.setItem("users", JSON.stringify(users));
 }
 
+function onEdit(td) {
+
+    const userform = document.getElementById("reg-form")
+    userform.reset()
+
+    const selectedRow = td.parentElement.parentElement;
+    editUserIndex = selectedRow.rowIndex - 1;
+    document.getElementById("user-name").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("user-age").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("user-gender").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("contact-number").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("email-id").value = selectedRow.cells[4].innerHTML;
+    document.getElementById("school-name").value = selectedRow.cells[5].innerHTML;
+    document.getElementById("High-school-cgpa").value = selectedRow.cells[6].innerHTML;
+
+    const primaryLang = selectedRow.cells[7].innerText.trim();
+    document.querySelectorAll('input[name="primaryLanguage"]').forEach(radio => {
+        radio.checked = (radio.value === primaryLang);
+    });
 
 
-function getData() {
-    return JSON.parse(localStorage.getItem("users")) || [];
+    const Value = selectedRow.cells[8].innerText;
+    const seperatdValue = Value.split(",").map(v => v.trim());
+    const checkboxes = document.querySelectorAll('input[name="languagesKnown"]');
+    checkboxes.forEach(checkbox => {
+        if (seperatdValue.includes(checkbox.value)) {
+            checkbox.checked = true;
+        }
+    });
+
+
+    document.getElementById("submit-btn").innerText = "Update";
+    userFormValidation();
+
 
 }
-
-// Save data to LocalStorage
-function saveData(data) {
-    localStorage.setItem("users", JSON.stringify(data));
+function onDelete(td) {
+    const row = td.parentElement.parentElement;
+    const rowIndex = row.rowIndex - 1;
+    const confirmDelete = confirm('Are you sure to delete this record ?');
+    if (confirmDelete) {
+        let users = JSON.parse(localStorage.getItem("users")) || [];
+        users.splice(rowIndex, 1);
+        localStorage.setItem("users", JSON.stringify(users));
+        document.getElementById("user-table").deleteRow(row.rowIndex);
+    }
+    else {
+        return;
+    }
 }
 
 
+// window.addEventListener("beforeunload", warnBeforeReload);
+// function warnBeforeReload(event) {
+//     event.preventDefault();
+//     event.returnValue = '';
+// }
 
-function editUser(index) {
-    const user = getData()[index];
-    document.getElementById("name").value = user.name;
-    document.getElementById("email").value = user.email;
-    document.getElementById("editIndex").value = index;
-    document.getElementById("formTitle").innerText = "Edit User";
+
+function isDuplicateEmail(editIndex) {
+    // const emailError = document.getElementById("email-error");
+    const inputEmail = document.getElementById("email-id").value.trim();
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    for (let i = 0; i < users.length; i++) {
+
+
+        if (editIndex !== null && i === editIndex) {
+            continue;
+        }
+
+        if (users[i].email.toLowerCase() === inputEmail.toLowerCase()) {
+            return true;
+
+
+        }
+    }
+
+    return false;
 }
 
+function getPrimaryLanguage() {
 
+    const radios = [
+        document.getElementById("lang-en"),
+        document.getElementById("lang-hi"),
+        document.getElementById("lang-gu")
+    ];
 
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            return radios[i].value;
+        }
+    }
+
+    return "";
+}
+function getLanguagesKnown() {
+    const languages = [];
+
+    if (document.getElementById("chk-en").checked) languages.push("English");
+    if (document.getElementById("chk-hi").checked) languages.push("Hindi");
+    if (document.getElementById("chk-gu").checked) languages.push("Gujarati");
+    if (document.getElementById("chk-mr").checked) languages.push("Marathi");
+
+    return languages;
+}
+
+function validatePrimaryLanguage() {
+    const radios = document.getElementsByName("primaryLanguage");
+    const error = document.getElementById("primary-lang-error");
+    userFormValidation()
+    let isChecked = false;
+
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            isChecked = true;
+            break;
+        }
+    }
+
+    if (!isChecked) {
+        error.textContent = "Please select a primary language";
+        return false;
+    }
+
+    error.textContent = "";
+    return true;
+}
+
+function validateLanguagesKnown() {
+    const error = document.getElementById("languages-error");
+    userFormValidation()
+    if (
+        document.getElementById("chk-en").checked ||
+        document.getElementById("chk-hi").checked ||
+        document.getElementById("chk-gu").checked ||
+        document.getElementById("chk-mr").checked) {
+        error.textContent = "";
+        console.log("knownLang true")
+        return true;
+    }
+    else {
+        error.textContent = "Select at least one language";
+        console.log("knownLang false")
+        return false;
+    }
+}
+function setPrimaryLanguage(value) {
+    const radios = [
+        document.getElementById("lang-en"),
+        document.getElementById("lang-hi"),
+        document.getElementById("lang-gu")
+    ];
+
+    for (let i = 0; i < radios.length; i++) {
+        radios[i].checked = (radios[i].value === value);
+    }
+}
+
+function setLanguagesKnown(languages) {
+    document.getElementById("chk-en").checked = languages.includes("English");
+    document.getElementById("chk-hi").checked = languages.includes("Hindi");
+    document.getElementById("chk-gu").checked = languages.includes("Gujarati");
+    document.getElementById("chk-mr").checked = languages.includes("Marathi");
+}
+
+function validatePrimaryLanguage() {
+
+    const English = document.getElementById("lang-en").checked
+    const hindi = document.getElementById("lang-hi").checked
+    const gujarati = document.getElementById("lang-gu").checked
+    const primaryLangError = document.getElementById("primary-lang-error")
+    userFormValidation()
+
+    if (English || hindi || gujarati) {
+        primaryLangError.innerHTML = "";
+        console.log("primary lang true")
+        return true
+    }
+    else {
+        primaryLangError.innerHTML = "Please Select Your Primary Langauge"
+        console.log("primary lang false")
+        return false
+    }
+}
 
